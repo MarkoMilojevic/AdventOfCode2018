d3bfeda130c05a555a640e7e0825a6741e4f28ea
Marko Milojevic <milojevicmarko92@gmail.com>
2018-12-04 21:09:30 +0100

    Refactor day 2.

--- Day2/StringExtensions.cs
+++ Day2/StringExtensions.cs
@@ -19,14 +19,9 @@ namespace Day2
         private static Option<int> DifferingIndex(this string s1, string s2) =>
             s1.DifferingIndexes(s2).ToArray().SingleOrNone();
 
-        private static IEnumerable<int> DifferingIndexes(this string s1, string s2)
-        {
-            string first = s1.Length <= s2.Length ? s1 : s2;
-            string second = s1.Length <= s2.Length ? s2 : s1;
-
-            return first
-                   .Select((_, index) => first[index] != second[index] ? index : -1)
-                   .Where(index => index != -1);
-        }
+        private static IEnumerable<int> DifferingIndexes(this string first, string second) =>
+            first
+                .Select((_, index) => index < second.Length && first[index] != second[index] ? index : -1)
+                .Where(index => index != -1);
     }
 }
--- FunctionalExtensions/EnumerableExtensions.cs
+++ FunctionalExtensions/EnumerableExtensions.cs
@@ -2,14 +2,7 @@
 {
     public static class EnumerableExtensions
     {
-        public static Option<T> SingleOrNone<T>(this T[] source)
-        {
-            if (source == null)
-                return None.Value;
-
-            return source.Length == 1
-                       ? (Option<T>)source[0]
-                       : None.Value;
-        }
+        public static Option<T> SingleOrNone<T>(this T[] source) =>
+            source?.Length == 1 ? (Option<T>) source[0] : None.Value;
     }
 }
