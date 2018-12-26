ddcea067c85b8d39fa778b47c6350d80c1da071b
Marko Milojevic <marko.milojevic@nultien.rs>
2018-12-26 11:01:36 +0100

    Refactor.

---
+++ Day1/EnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day1
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> Circular<T>(this IEnumerable<T> sequence)
+        {
+            List<T> list = sequence.ToList();
+
+            while (true)
+            {
+                foreach (T item in list)
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
--- Day1/FrequencyExtensions.cs
+++ Day1/FrequencyExtensions.cs
@@ -5,27 +5,26 @@ namespace Day1
 {
     public static class FrequencyExtensions
     {
-        public static Frequency Calibrate(this Frequency frequency, int delta) =>
+        public static Frequency Calibrate(this Frequency frequency, int delta) => 
             new Frequency(frequency + delta);
 
-        public static Frequency Calibrate(this Frequency frequency, int[] deltas) =>
+        public static Frequency Calibrate(this Frequency frequency, int[] deltas) => 
             deltas.Aggregate(frequency, (current, delta) => current.Calibrate(delta));
 
         public static Frequency CalibrateUntilRepeated(this Frequency frequency, int[] deltas)
         {
             var seen = new HashSet<Frequency> { frequency };
 
-            while (true)
+            foreach (int delta in deltas.Circular())
             {
-                foreach (int delta in deltas)
-                {
-                    frequency = frequency.Calibrate(delta);
-                    if (seen.Contains(frequency))
-                        return frequency;
+                frequency = frequency.Calibrate(delta);
+                if (seen.Contains(frequency))
+                    return frequency;
 
-                    seen.Add(frequency);
-                }
+                seen.Add(frequency);
             }
+
+            return frequency;
         }
     }
 }
