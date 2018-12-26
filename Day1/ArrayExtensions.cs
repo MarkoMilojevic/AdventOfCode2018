40fdbdb18d7e0bcafc64059a24b82fff8165d205
Marko Milojevic <marko.milojevic@nultien.rs>
2018-12-26 11:04:19 +0100

    Refactor.

---
+++ Day1/ArrayExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Day1
+{
+    public static class ArrayExtensions
+    {
+        public static IEnumerable<T> Circular<T>(this T[] array)
+        {
+            while (true)
+            {
+                foreach (T item in array)
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
